ab5767858c16c21a5e10af600811a19558674e07
rongitbon <rongitbon@gmail.com>
2018-10-25 17:26:18 +0300

    adding port

--- server.js
+++ server.js
@@ -61,6 +61,6 @@ app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)});
 
 app.post('/profile/:id', (req,res) => {profile.handleProfile(req, res, db)});
 
-app.listen(3000, () => {
-	console.log('app is running on port 3000');
+app.listen(process.env.PORT || 3000 , () => {
+	console.log(`app is running on port ${process.env.PORT}`);
 });
