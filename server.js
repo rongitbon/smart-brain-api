b992e62e660e5ce9166ec8be5558c1b676739e61
rongitbon <rongitbon@gmail.com>
2018-10-25 17:01:24 +0300

    The server side in the face recognition project

---
+++ server.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register.js');
+const signin = require('./controllers/signin.js');
+const profile = require('./controllers/profile.js');
+const image = require('./controllers/image.js');
+
+const db = knex({
+	client:'pg',
+	connection: {
+		host: '127.0.0.1',
+		user: 'postgres',
+		password: 'tEst^7*',
+		database: 'smart-brain'
+	}
+});
+
+
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const database = {
+	users: [
+		{
+			id:'123',
+			name: 'John',
+			email: 'john@gmail.com',
+			password : 'cookies',
+			entries: 0,
+			joined: new Date()
+		},
+		{
+			id:'124',
+			name: 'Sally',
+			email: 'sally@gmail.com',
+			password : 'bananas',
+			entries: 0,
+			joined: new Date()
+		},
+	]
+}
+
+app.get('/', (req, res) => {
+	res.send(database.users);
+});
+
+app.post('/signin' ,(req, res) => {signin.handleSignin(req, res, db, bcrypt)});
+
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt)});
+
+app.put('/image', (req, res) => {image.handleImage(req, res, db)});
+
+app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)});
+
+app.post('/profile/:id', (req,res) => {profile.handleProfile(req, res, db)});
+
+app.listen(3000, () => {
+	console.log('app is running on port 3000');
+});
